9eda93b6b4129a12c72bf24fd1cf18bf60010bde
webdeve1oper <akhmedinov94@gmail.com>
2021-04-05 14:26:33 +0600

    language files

---
+++ resources/lang/ru/home.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'test' => 'Russian',
+    'menu-projects' => 'О Проекте',
+    'menu-news' => 'Новости',
+    'menu-faq' => 'Вопрос-ответ',
+    'menu-contacts' => 'Контакты',
+    'menu-testimonials' => 'Отызвы',
+    'share' => 'Поделиться ',
+    'sign-in' => 'Войти ',
+    'sign-up' => 'Регистрация',
+    'fonds' => 'Реестр благотворительных организаций',
+    'helps' => 'Им нужна помощь',
+    'help-project' => 'Помочь проекту',
+    'map-charity' => 'Карта благотворительности',
+    'we-support' => 'Нас поддержали',
+    'site-search' => 'Поиск по сайту',
+    'call-center' => 'Call-центр',
+    'copyright' => 'Все права на сайт принадлежат',
+    'copyright-dev' => 'Создание и разработка сайтов',
+    'social' => 'Поделиться в соц. сетях',
+    'share-social' => 'Социальные сети',
+    'main-h1' => 'Единая приемная всех благотворительных организаций Казахстана',
+    'main-h2' => 'Реестр благотворительных организаций',
+    'main-descr' => 'Реестр поможет тебе найти организацию, которая решит твою проблему, помочь тем, кто оказался в тяжелой жизненной ситуации, и найти благотворительную организацию, достойную поддержки',
+    'apply-for-assistance' => 'Подать заявку на получение помощи',
+    'partic' => 'Принять участие',
+];
