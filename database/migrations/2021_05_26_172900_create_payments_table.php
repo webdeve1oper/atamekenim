f2c382e9f4ec0d596a5afef651308ee2487dc2e9
web developer <akhmedinov94@gmail.com>
2021-05-26 17:41:44 +0600

    payments

---
+++ database/migrations/2021_05_26_172900_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('fond_id');
+            $table->foreign('fond_id', 'fond_id_payments_foreign')->references('id')->on('fonds');
+            $table->string('fio')->nullable();
+            $table->boolean('anonim')->default(false);
+            $table->string('amount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
--- resources/views/frontend/fond/payments/cloudpayment.blade.php
+++ resources/views/frontend/fond/payments/cloudpayment.blade.php
@@ -14,12 +14,15 @@
                         <input type="hidden" name="fond_id" value="{{$fond->id}}">
                         <div class="form-group mb-4">
                             <label for="">Введите Ваше ФИО:</label>
-                            <input type="name" value="ФИО" class="form-control">
+                            <input type="text" id="fio_cloud" value="ФИО" class="form-control">
                         </div>
-                        <input type="text" name="amount" value="">
+                        <div class="form-group mb-4">
+                            <input type="checkbox" id="anomim_cloud" value="ФИО" class="form-control"><label for=""> Хочу отстаться анонимом</label>
+                        </div>
+                        <input type="hidden" name="amount" id="amount_cloud" value="">
                     </div>
                     <input type="hidden" name="DESC_ORDER" value="Помощь фонду {{$fond['title_'.app()->getLocale()] ?? $fond['title_ru']}}">
-                    <button class="btn-default red" onclick="payHandler();javascript:void(null);">
+                    <button class="btn-default red" onclick="payHandler($('#amount_cloud').val(), $('#fio_cloud').val(), $('#anomim_cloud').is(':checked'));javascript:void(null);">
                         <img src="/img/help.svg" alt=""> {{trans('fonds-page.supp-org')}}
                     </button>
                 {{--</form>--}}
@@ -29,9 +32,18 @@
 </div>
 <script src="https://widget.cloudpayments.ru/bundles/cloudpayments"></script>
 <script type="text/javascript">
-        function payHandler() {
+        function payHandler(amount, fio, anonim) {
+            var amount = parseFloat(amount);
             var widget = new cp.CloudPayments();
             var receipt = {
+                taxationSystem: 0,
+                email: 'web@conversion.kz', //e-mail покупателя, если нужно отправить письмо с чеком
+                phone: '', //телефон покупателя в любом формате, если нужно отправить сообщение со ссылкой на чек
+                customerInfo: fio,
+                isBso: false, //чек является бланком строгой отчетности
+            };
+
+            var old = {
                 Items: [//товарные позиции
                     {
                         label: 'Наименование товара 3', //наименование товара
@@ -62,14 +74,21 @@
             widget.charge({ // options
                     publicId: '{{config('services.cloud_payments.public_id')}}', //id из личного кабинета
                     description: 'Подписка на ежемесячные платежи', //назначение
-                    amount: 10, //сумма
+                    amount: amount, //сумма
                     currency: 'KZT', //валюта
-                    invoiceId: '2', //номер заказа  (необязательно)
+                    invoiceId: '3', //номер заказа  (необязательно)
                     accountId: 'web2@conversion.kz', //идентификатор плательщика (обязательно для создания подписки)
                     data: data
                 },
                 function (options) { // success
-                    console.log(options);
+                    $.ajax({
+                      url: '{{route('donation_to_fond')}}',
+                      method: 'POST',
+                        data: {'fio': options.data.customerInfo, 'amount': options.amount, 'fond_id': {{$fond->id}},
+                        success: function(data){
+                          {{--window.location.href= '{{route('success_payment')}}';--}}
+                        }
+                    });
                     //действие при успешной оплате
                 },
                 function (reason, options) { // fail
